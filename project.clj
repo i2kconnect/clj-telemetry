668ccb8f19061d6788bfb0cdc6b0c6010fd26363
ejschoen <ejschoen@i2kconnect.com>
2023-09-21 08:10:45 -0500

    Update io.opentelemetry versions

--- project.clj
+++ project.clj
@@ -4,13 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.opentelemetry/opentelemetry-api "1.9.0"]
-                 [io.opentelemetry/opentelemetry-sdk "1.9.0"]
-                 [io.opentelemetry/opentelemetry-exporter-jaeger "1.9.0"]
-                 [io.opentelemetry/opentelemetry-exporter-otlp "1.9.0"]
-                 [io.opentelemetry/opentelemetry-exporter-zipkin "1.9.0"]
+                 [io.opentelemetry/opentelemetry-api "1.14.0"]
+                 [io.opentelemetry/opentelemetry-sdk "1.14.0"]
+                 [io.opentelemetry/opentelemetry-exporter-jaeger "1.14.0"]
+                 [io.opentelemetry/opentelemetry-exporter-otlp "1.14.0"]
+                 [io.opentelemetry/opentelemetry-exporter-zipkin "1.14.0"]
                  ;;[io.grpc/grpc-protobuf "1.34.1"]
                  ;;[io.grpc/grpc-netty-shaded "1.34.1"]
                  ]
-  :profiles {:test {:dependencies [[io.opentelemetry/opentelemetry-exporters-inmemory "0.9.1"]]}}
+  :profiles {:test {:dependencies [[io.opentelemetry/opentelemetry-exporters-inmemory "0.9.1"]
+                                   [io.grpc/grpc-protobuf "1.46.0"]
+                                   [io.grpc/grpc-netty-shaded "1.46.0"]
+                                   ]}}
   :repl-options {:init-ns telemetry.tracing})
