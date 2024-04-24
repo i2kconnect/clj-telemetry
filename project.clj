cc8a520acc20ac65795501e839ab709456f3b51a
ejschoen <ejschoen@i2kconnect.com>
2024-04-24 08:50:31 -0500

    Update opentelemetry versions

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.clojars.ejschoen/clj-telemetry "0.3.0-SNAPSHOT"
+(defproject org.clojars.ejschoen/clj-telemetry "0.3.1-SNAPSHOT"
   :description "A Clojure library designed to wrap OpenTelemetry Java API"
   :url "https://github.com/tendant/clj-telemetry"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.opentelemetry/opentelemetry-api "1.14.0"]
-                 [io.opentelemetry/opentelemetry-sdk "1.14.0"]
-                 [io.opentelemetry/opentelemetry-exporter-jaeger "1.14.0"]
-                 [io.opentelemetry/opentelemetry-exporter-otlp "1.14.0"]
-                 [io.opentelemetry/opentelemetry-exporter-zipkin "1.14.0"]
+                 [io.opentelemetry/opentelemetry-api "1.37.0"]
+                 [io.opentelemetry/opentelemetry-sdk "1.37.0"]
+                 [io.opentelemetry/opentelemetry-exporter-jaeger "1.34.1"]
+                 [io.opentelemetry/opentelemetry-exporter-otlp "1.37.0"]
+                 [io.opentelemetry/opentelemetry-exporter-zipkin "1.37.0"]
                  ;;[io.grpc/grpc-protobuf "1.34.1"]
                  ;;[io.grpc/grpc-netty-shaded "1.34.1"]
                  ]
